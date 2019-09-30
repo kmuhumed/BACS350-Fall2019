db86082525eacfcc1c85a62368cd511c6a95e1c4
spillman66 <Spil6540@bears.unco.edu>
2019-09-30 15:06:49 -0600

    push

--- bacs350/docman/index.php
+++ bacs350/docman/index.php
@@ -1,36 +1,44 @@
-<?php
-
-    // Bring in key functions
-    require 'views.php';
-    require 'files.php';
 
 
-    // ------------------------------------------
-    // FILES
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Madi's Docman</title>
+                    <link rel="icon" type="image/x-icon" href="/bacs350/favicon.ico">
+                   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                    <link rel="stylesheet" href="style.css">
+                </head>
+                <body>
+                    <header>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-sm-8">
+                                    <h1>Madi's Notes</h1>
+                                    <h2>Document Manager</h2>
+                                </div>
+                                <div class="logo col-sm-4">
+                                    <div class="pull-right">
+                                        <img class="img-rounded img-responsive"
+                                        src="spillman%20logo.png"
+                                        alt="UNC Bear" width="150px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
 
-    // Show the directories in "bacs350/docman/docs"
-    $path = 'docs';
-    
-
-    // Get the files in the "bacs350/docman/docs" and create HTML list
-    $files = get_file_list($path);
-    $file_list = render_links($files);
-   
-
-    // ------------------------------------------
-    // PAGE
-
-    // Page Intro
-    $text = render_markdown('Docs.md');
-    
-
-    // Card with docs
-    $title = 'Document List';
-    $body = $file_list;
-    $card = render_card($title, $body);
-
-
-    // Display the HTML in the page
-    echo render_page('Seaman Notes', "Document Manager", $card);
-
-?>
+                    <main>
+                        
+            <div class="card">
+                <div class="card-header">
+                    Document List
+                </div>
+                <div class="card-body card-padding">
+                    <ul><li><a href="doc.php?doc=docs/ClassPrep.md">docs/ClassPrep.md</a></li><li><a href="doc.php?doc=docs/DocMan.md">docs/DocMan.md</a></li><li><a href="doc.php?doc=docs/Engineering.md">docs/Engineering.md</a></li><li><a href="doc.php?doc=docs/Markdown.md">docs/Markdown.md</a></li><li><a href="doc.php?doc=docs/Test.md">docs/Test.md</a></li></ul>
+                </div>
+            </div>
+        
+                    </main>
+                </body>
+            </html>
