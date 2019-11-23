5875310a2c0745328dbc8427729a157ea070df3d
Mark Seaman <Mark.Seaman@imac.net>
2019-11-23 10:24:46 -0700

    Revert "fskdfjaokdf"
    This reverts commit 1e64cbfd72d2a4cbe125bbcaec135476eab5b13c.

--- bacs350/index.php
+++ bacs350/index.php
@@ -20,7 +20,10 @@
         </p>
         <p> 
             Different rooms within this PHP app will contain different types of info.
-        </p
+        </p>
+        <p>
+            The source code is available at <a href="https://github.com/Mark-Seaman/UNC-BACS350-Demo/tree/master/bacs350">BACS 350 Source Code</a>.
+        </p>
         
         <ul>
             <li>
--- bacs350/slides/docs/slides.sql
+++ bacs350/slides/docs/slides.sql
@@ -1,68 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Nov 11, 2019 at 02:55 PM
--- Server version: 5.7.23-23
--- PHP Version: 7.2.7
+# Slide Show SQL code
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+## Database table
+
+    CREATE TABLE slides (
+        id          int(3) NOT NULL AUTO_INCREMENT,
+        title       varchar(100) NOT NULL,
+        body        text         NOT NULL,
+        PRIMARY KEY (id)
+    );
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+## Connection Settings  - PHP Variables 
 
---
--- Database: `spillma4_slides`
---
+    $dbname = 'uncobacs_350';
+    $username = 'uncobacs_350';
+    $password = 'BACS_350';
+    $port = '3306';
+    $host = "localhost:$port";
 
--- --------------------------------------------------------
 
---
--- Table structure for table `slides`
---
+## CREATE
 
-CREATE TABLE `slides` (
-  `id` int(3) NOT NULL,
-  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `body` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    INSERT INTO slides (title, body) 
+    VALUES (:title, :body);
 
---
--- Dumping data for table `slides`
---
 
-INSERT INTO `slides` (`id`, `title`, `body`) VALUES
-(1, 'Test Slide', 'This is a test slide I am adding to my slide project. Let\'s see if it works!'),
-(2, 'Test Slide', 'This is a test slide I am adding to my slide project. Let\'s see if it works!');
+## READ
 
---
--- Indexes for dumped tables
---
+    SELECT * FROM slides;
 
---
--- Indexes for table `slides`
---
-ALTER TABLE `slides`
-  ADD PRIMARY KEY (`id`);
+    SELECT * FROM slides WHERE id = :id; 
 
---
--- AUTO_INCREMENT for dumped tables
---
+    foreach (slides as row) {
+        row['id']
+        row['title']
+        row['body']
+    }
 
---
--- AUTO_INCREMENT for table `slides`
---
-ALTER TABLE `slides`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+## UPDATE
+
+    SELECT * FROM slides WHERE id = :id;
+
+    UPDATE slides 
+    SET title=:title, body=:body
+    WHERE id = :id;
+
+
+## DELETE
+
+    DELETE from slides WHERE id = :id;
+
